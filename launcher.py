26f56af35a65935eadd339bc9de550a2e069f3f3
SLAC fphysics <zack@slac.stanford.edu>
2023-12-13 10:20:58 -0800

    switch names of launch/launcher for consistency

--- launcher.py
+++ launcher.py
@@ -2,20 +2,20 @@
 # desired display to launch is 1st (and only) arg
 
 from sys import argv
-from core import launcher
+from core import launch
 
 def show_help():
     print('Usage:')
     print('  $ python launcher.py [CUD_NAME]')
     print('  where [CUD_NAME] is one of:')
-    for name in launcher.CUD_names(): print(f'  * {name}')
+    for name in launch.CUD_names(): print(f'  * {name}')
     print()
 
 def main():
     try:
         target = argv[1]
         print(f' -> Launching FACET-II {target} CUD')
-        launcher.run_CUD(target)
+        launch.run_CUD(target)
     except (KeyError, IndexError):
         print('Invalid arguments provided.')
         show_help()
