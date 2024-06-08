d401b6e3c38dabc1f663097cdbc386f9bef27c69
SLAC fphysics <zack@slac.stanford.edu>
2024-06-07 18:59:04 -0700

    convert BLEN fb actuator labels to chirp in MeV

--- long_FB/hist.py
+++ long_FB/hist.py
@@ -35,30 +35,23 @@ class F2longHist(Display):
 
         self.ui.L0_energy.getAxis('Axis 1').linkedView().setYRange(-1,1, 1.1)
         self.ui.L0_energy.getAxis('Axis 2').linkedView().setYRange(28, 40)
-        self.ui.L0_energy.plot_by_timestamps = False
 
         self.ui.L1_energy.getAxis('Axis 1').linkedView().setYRange(-2.2, 2.2)
         self.ui.L1_energy.getAxis('Axis 2').linkedView().setYRange(130, 160)
-        self.ui.L1_energy.plot_by_timestamps = False
 
         self.ui.L1_blen.getAxis('Axis 1').linkedView().setYRange(0, 8000)
         self.ui.L1_blen.getAxis('Axis 2').linkedView().setYRange(-32,2)
-        self.ui.L1_energy.plot_by_timestamps = False
 
         self.ui.L2_energy.getAxis('Axis 1').linkedView().setYRange(-4.4, 4.4)
         self.ui.L2_energy.getAxis('Axis 2').linkedView().setYRange(-180, 180)
-        self.ui.L2_energy.plot_by_timestamps = False
 
         self.ui.L2_blen.getAxis('Axis 1').linkedView().setYRange(0, 11000)
         self.ui.L2_blen.getAxis('Axis 2').linkedView().setYRange(-60, -20)
-        self.ui.L2_energy.plot_by_timestamps = False
 
         self.ui.L3_energy.getAxis('Axis 1').linkedView().setYRange(-4.4, 4.4)
         self.ui.L3_energy.getAxis('Axis 2').linkedView().setYRange(-180, 180)
-        self.ui.L3_energy.plot_by_timestamps = False
 
         self.ui.L3_blen.getAxis('Axis 1').linkedView().setYRange(0, 300)
-        self.ui.L3_energy.plot_by_timestamps = False
 
         return
 
