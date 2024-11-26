8f83618c3320a9ca3ed4113c9ae09c890c49255e
SLAC fphysics <zack@slac.stanford.edu>
2024-11-25 17:00:30 -0800

    add xwin forwarding to cud manager launch cmd

--- manager/launcher.sh
+++ manager/launcher.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-ssh fphysics@facet-srv02 pydm --hide-nav-bar --hide-menu-bar --hide-status-bar /usr/local/facet/tools/python/F2-CUDs/manager/cud_manager.py
+ssh -X fphysics@facet-srv02 pydm --hide-nav-bar --hide-menu-bar --hide-status-bar /usr/local/facet/tools/python/F2-CUDs/manager/cud_manager.py
