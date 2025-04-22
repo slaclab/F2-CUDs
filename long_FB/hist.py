7a47e420369188fca1cb28dc5cd12218d76a5e82
SLAC fphysics <zack@slac.stanford.edu>
2025-04-21 17:07:17 -0700

    modify long FB history plots to be stacked

--- long_FB/hist.py
+++ long_FB/hist.py
@@ -33,25 +33,31 @@ class F2longHist(Display):
     def __init__(self, parent=None, args=None):
         super(F2longHist, self).__init__(parent=parent, args=args)
 
-        self.ui.L0_energy.getAxis('Axis 1').linkedView().setYRange(-1,1, 1.1)
-        self.ui.L0_energy.getAxis('Axis 2').linkedView().setYRange(28, 40)
+        # self.ui.L0_energy.getAxis('Axis 1').linkedView().setYRange(-1,1, 1.1)
+        # self.ui.L0_energy.getAxis('Axis 2').linkedView().setYRange(28, 40)
 
-        self.ui.L1_energy.getAxis('Axis 1').linkedView().setYRange(-2.2, 2.2)
-        self.ui.L1_energy.getAxis('Axis 2').linkedView().setYRange(130, 160)
+        # self.ui.L1_energy.getAxis('Axis 1').linkedView().setYRange(-2.2, 2.2)
+        # self.ui.L1_energy.getAxis('Axis 2').linkedView().setYRange(130, 160)
 
-        self.ui.L1_blen.getAxis('Axis 1').linkedView().setYRange(0, 8000)
-        self.ui.L1_blen.getAxis('Axis 2').linkedView().setYRange(-32,2)
+        # self.ui.L1_blen.getAxis('Axis 1').linkedView().setYRange(0, 8000)
+        # self.ui.L1_blen.getAxis('Axis 2').linkedView().setYRange(-32,2)
 
-        self.ui.L2_energy.getAxis('Axis 1').linkedView().setYRange(-4.4, 4.4)
-        self.ui.L2_energy.getAxis('Axis 2').linkedView().setYRange(-180, 180)
+        # self.ui.L2_energy.getAxis('Axis 1').linkedView().setYRange(-4.4, 4.4)
+        # self.ui.L2_energy.getAxis('Axis 2').linkedView().setYRange(-180, 180)
 
-        self.ui.L2_blen.getAxis('Axis 1').linkedView().setYRange(0, 11000)
-        self.ui.L2_blen.getAxis('Axis 2').linkedView().setYRange(-60, -20)
+        # self.ui.L2_blen.getAxis('Axis 1').linkedView().setYRange(0, 11000)
+        # self.ui.L2_blen.getAxis('Axis 2').linkedView().setYRange(-60, -20)
 
-        self.ui.L3_energy.getAxis('Axis 1').linkedView().setYRange(-4.4, 4.4)
-        self.ui.L3_energy.getAxis('Axis 2').linkedView().setYRange(-180, 180)
+        # self.ui.L3_energy.getAxis('Axis 1').linkedView().setYRange(-4.4, 4.4)
+        # self.ui.L3_energy.getAxis('Axis 2').linkedView().setYRange(-180, 180)
 
-        self.ui.L3_blen.getAxis('Axis 1').linkedView().setYRange(0, 300)
+        # self.ui.L3_blen.getAxis('Axis 1').linkedView().setYRange(0, 300)
+
+        for plot in [
+            self.ui.L0_energy, self.ui.L1_energy, self.ui.L1_blen,
+            self.ui.L2_energy, self.ui.L2_blen, self.ui.L3_energy,
+            ]:
+            plot.hideAxis('bottom')
 
         return
 
---
+++ long_FB/histplots.py
@@ -0,0 +1,35 @@
+import os, sys
+from os import path
+from sys import exit
+import time
+import numpy as np
+
+from epics import caget
+
+import pydm
+from pydm import Display
+from pydm.widgets.channel import PyDMChannel
+
+SELF_PATH = path.dirname(path.abspath(__file__))
+REPO_ROOT = path.join(*path.split(SELF_PATH)[:-1])
+
+sys.path.append(REPO_ROOT)
+
+class F2longHist(Display):
+
+    def __init__(self, parent=None, args=None):
+        super(F2longHist, self).__init__(parent=parent, args=args)
+
+        for plot in [
+            self.ui.L0_energy, self.ui.L1_energy, self.ui.L1_blen,
+            self.ui.L2_energy, self.ui.L2_blen, self.ui.L3_energy,
+            ]:
+            plot.hideAxis('bottom')
+
+        self.ui.L1_blen.getAxis('Axis 2').linkedView().setYRange(-40,0)
+        self.ui.L2_blen.getAxis('Axis 2').linkedView().setYRange(-70,0)
+
+        return
+
+    def ui_filename(self):
+        return os.path.join(SELF_PATH, 'histplots_stacked.ui')
