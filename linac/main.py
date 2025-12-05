d40820fca198e2a741c86d5330ec794e02bdbf35
SLAC fphysics <zack@slac.stanford.edu>
2025-12-04 17:33:58 -0800

    use shared widgets, refactor measurement timestamp setup

--- linac/main.py
+++ linac/main.py
@@ -2,6 +2,7 @@ import sys
 from os import path
 from epics import get_pv
 from datetime import datetime as dt
+from functools import partial
 from pydm import Display
 from pydm.widgets.label import PyDMLabel
 from pydm.widgets.channel import PyDMChannel
@@ -11,29 +12,19 @@ from PyQt5.QtGui import QFont
 SELF_PATH = path.dirname(path.abspath(__file__))
 REPO_ROOT = path.join(*path.split(SELF_PATH)[:-1])
 sys.path.append(REPO_ROOT)
-from core.common import bitStatusLabel
+from widgets.bitStatusLabel import bitStatusLabel
+from widgets.SCPSteeringFBIndicator import SCPSteeringFBIndicator
 
-
-# TIMESTAMP PVs
-# SIOC:SYS1:ML03:CA951 - CA954
-
-# emit/msmt PVs
-# SIOC:SYS1:ML03:AO951 - AO971
 
 PV_L0_MSMT     = 'PROF:IN10:571:EMITN_X'
-PV_L2_MSMT     = 'WIRE:LI11:614:XRMS'
-# PV_L2_TS       = 'SIOC:SYS1:ML03:CA951'
-PV_L3_MSMT     = 'WIRE:LI19:144:XRMS'
-# PV_L3_TS       = 'SIOC:SYS1:ML03:CA952'
+PV_L2_MSMT     = 'WIRE:LI11:614:EMITN_X'
+PV_L3_MSMT     = 'WIRE:LI19:144:EMITN_X'
 PV_IPWS1_MSMT  = 'WIRE:LI20:3179:XRMS'
-# PV_IPWS1_TS    = 'SIOC:SYS1:ML03:CA953'
 PV_IPBlen_MSMT = 'CAMR:LI20:107:BLEN'
-# PV_IPBlen_TS   = 'SIOC:SYS1:ML03:CA954'
 
 PV_FB_ENABLE = 'SIOC:SYS1:ML00:AO856'
 
 STAT_REG  = QFont('Sans Serif', 18)
-
 
 STYLE_GREEN = """
 PyDMLabel {
@@ -58,41 +49,53 @@ PyDMLabel[alarmSensitiveBorder="true"][alarmSeverity="0"] {
 }
 """
 
-# stupid magic numbers because I can't find a FBCK HSTA bit decoder
-HSTA_FBCK_ON = 268601505
-HSTA_FBCK_COMP = 268599457
-
 
 class F2_CUD_linac(Display):
 
     def __init__(self, parent=None, args=None):
         super(F2_CUD_linac, self).__init__(parent=parent, args=args)
 
-        self.L0_msmt     = PyDMChannel(address=PV_L0_MSMT, value_slot=self.msmt_ts_L0)
-        self.L2_msmt     = PyDMChannel(address=PV_L2_MSMT, value_slot=self.msmt_ts_L2)
-        self.L3_msmt     = PyDMChannel(address=PV_L3_MSMT, value_slot=self.msmt_ts_L3)
-        self.IPWS1_msmt  = PyDMChannel(address=PV_IPWS1_MSMT, value_slot=self.msmt_ts_IPWS1)
-        self.IPBlen_msmt = PyDMChannel(address=PV_IPBlen_MSMT, value_slot=self.msmt_ts_IPBlen)
-        for ch in [self.L0_msmt, self.L2_msmt, self.L3_msmt, self.IPWS1_msmt, self.IPBlen_msmt]:
-            ch.connect()
-
+        # connect to emittance & S20 measurement PVs to update timestamps
         self.L0_msmt_PV     = get_pv(PV_L0_MSMT)
         self.L2_msmt_PV     = get_pv(PV_L2_MSMT)
         self.L3_msmt_PV     = get_pv(PV_L3_MSMT)
         self.IPWS1_msmt_PV  = get_pv(PV_IPWS1_MSMT)
         self.IPBlen_msmt_PV = get_pv(PV_IPBlen_MSMT)
 
+        self.L0_msmt = PyDMChannel(
+            address=PV_L0_MSMT,
+            value_slot=partial(self.msmt_ts, self.L0_msmt_PV, self.ui.ts_L0)
+            )
+        self.L2_msmt = PyDMChannel(
+            address=PV_L2_MSMT,
+            value_slot=partial(self.msmt_ts, self.L2_msmt_PV, self.ui.ts_L2)
+            )
+        self.L3_msmt = PyDMChannel(
+            address=PV_L3_MSMT,
+            value_slot=partial(self.msmt_ts, self.L3_msmt_PV, self.ui.ts_L3)
+            )
+        self.IPWS1_msmt = PyDMChannel(
+            address=PV_IPWS1_MSMT,
+            value_slot=partial(self.msmt_ts, self.IPWS1_msmt_PV, self.ui.ts_IPWS1)
+            )
+        self.IPBlen_msmt = PyDMChannel(
+            address=PV_IPBlen_MSMT,
+            value_slot=partial(self.msmt_ts, self.IPBlen_msmt_PV, self.ui.ts_IPBlen)
+            )
+        for ch in [self.L0_msmt, self.L2_msmt, self.L3_msmt, self.IPWS1_msmt, self.IPBlen_msmt]:
+            ch.connect()
+
         Qbunch_enable = bitStatusLabel(
             'SIOC:SYS1:ML03:AO502', word_length=1, bit=0, parent=self.ui.ind_qfb)
         Qbunch_enable.setGeometry(0,0,70,38)
         Qbunch_enable.setFont(STAT_REG)
+        # override default style for bitStatusLabel
         Qbunch_enable.onstyle = STYLE_GREEN
         Qbunch_enable.offstyle = STYLE_YELLOW
 
-
-        ind_LI11 = F2SteeringFeedbackIndicator(
+        ind_LI11 = SCPSteeringFBIndicator(
             'LI11:FBCK:26:HSTA', parent=self.ui.ind_l2steer)
-        ind_LI18 = F2SteeringFeedbackIndicator(
+        ind_LI18 = SCPSteeringFBIndicator(
             'LI18:FBCK:28:HSTA', parent=self.ui.ind_l3steer)
         ind_LI11.setFont(STAT_REG)
         ind_LI18.setFont(STAT_REG)
@@ -100,47 +103,12 @@ class F2_CUD_linac(Display):
         ind_LI18.setGeometry(0,0,70,38)
 
         self.setWindowTitle('FACET-II CUD: Linac')
-
-        return
+        return 
 
     def ui_filename(self):
         return path.join(SELF_PATH, 'main.ui')
 
-    def msmt_ts_L0(self, value=None, char_value=None):
-        self.msmt_ts(self.L0_msmt_PV, self.ui.ts_L0, value=value, char_value=char_value)
-
-    def msmt_ts_L2(self, value=None, char_value=None):
-        self.msmt_ts(self.L2_msmt_PV, self.ui.ts_L2, value=value, char_value=char_value)
-        
-    def msmt_ts_L3(self, value=None, char_value=None, **kw):
-        self.msmt_ts(self.L3_msmt_PV, self.ui.ts_L3, value=value, char_value=char_value)
-
-    def msmt_ts_IPWS1(self, value=None, char_value=None, **kw):
-        self.msmt_ts(self.IPWS1_msmt_PV, self.ui.ts_IPWS1, value=value, char_value=char_value)
-
-    def msmt_ts_IPBlen(self, value=None, char_value=None, **kw):
-        self.msmt_ts(self.IPBlen_msmt_PV, self.ui.ts_IPBlen, value=value, char_value=char_value)
-
     def msmt_ts(self, PV_msmt_obj, label_obj, value=None, char_value=None, **kw):
-        """ temp kludge to update L2, L3 & IP Blen measurement timestamps """
+        """ sets the text of <label_obj> to the update timestamp of PV_msmt_obj """
         ts_str = str(dt.fromtimestamp(PV_msmt_obj.timestamp).strftime('%d-%b-%Y %H:%M'))
         label_obj.setText(ts_str)
-
-
-class F2SteeringFeedbackIndicator(PyDMLabel):
-    """ checks FBCK hardware status to check for feedback enable/compute """
-
-    def __init__(self, init_channel, parent=None, args=None):
-        PyDMLabel.__init__(self, init_channel=init_channel, parent=parent)
-        self.setAlignment(Qt.AlignCenter)
-
-    def value_changed(self, new_value):
-        PyDMLabel.value_changed(self, new_value)
-        if new_value == HSTA_FBCK_ON:    
-            self.setText('ON')
-            self.setStyleSheet(STYLE_GREEN)
-        elif new_value == HSTA_FBCK_COMP:
-            self.setText('Comp.')
-            self.setStyleSheet(STYLE_YELLOW)
-        else:                            
-            self.setText('Off/Sample')
