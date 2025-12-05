c204c0edb09ed0a335a33d673822fa6f54241f92
SLAC fphysics <zack@slac.stanford.edu>
2025-12-04 17:34:21 -0800

    use shared widgets, refactor measurement timestamp setup

--- wfh/main.py
+++ wfh/main.py
@@ -19,27 +19,19 @@ REPO_ROOT = path.join(*path.split(SELF_PATH)[:-1])
 
 sys.path.append(REPO_ROOT)
 
-from klys_indicator import sbstIndicator, klysIndicator
-from core.common import bitStatusLabel
+from widgets.bitStatusLabel import bitStatusLabel
+from widgets.klystronStatusIndicator import sbstIndicator, klysIndicator
+from widgets.InvertedPyDMImage import InvertedPyDMImage
+from widgets.SCPSteeringFBIndicator import SCPSteeringFBIndicator
 
 SELF_PATH = path.dirname(path.abspath(__file__))
 
 
-# TIMESTAMP PVs
-# SIOC:SYS1:ML03:CA951 - CA954
-
-# emit/msmt PVs
-# SIOC:SYS1:ML03:AO951 - AO971
-
 PV_L0_MSMT     = 'PROF:IN10:571:EMITN_X'
-PV_L2_MSMT     = 'WIRE:LI11:614:XRMS'
-# PV_L2_TS       = 'SIOC:SYS1:ML03:CA951'
-PV_L3_MSMT     = 'WIRE:LI19:144:XRMS'
-# PV_L3_TS       = 'SIOC:SYS1:ML03:CA952'
+PV_L2_MSMT     = 'WIRE:LI11:614:EMITN_X'
+PV_L3_MSMT     = 'WIRE:LI19:144:EMITN_X'
 PV_IPWS1_MSMT  = 'WIRE:LI20:3179:XRMS'
-# PV_IPWS1_TS    = 'SIOC:SYS1:ML03:CA953'
 PV_IPBlen_MSMT = 'CAMR:LI20:107:BLEN'
-# PV_IPBlen_TS   = 'SIOC:SYS1:ML03:CA954'
 
 SELF_PATH = path.dirname(path.abspath(__file__))
 
@@ -52,34 +44,7 @@ KLYSTRONS = [str(i) for i in range(1,9)]
 # stations that don't exist
 NONEXISTANT_RFS = ['11-3', '14-7', '15-2', '19-7', '19-8']
 
-PV_FB_ENABLE = 'SIOC:SYS1:ML00:AO856'
-
-STYLE_GREEN = """
-PyDMLabel {
-  border-radius: 2px;
-}
-PyDMLabel[alarmSensitiveBorder="true"][alarmSeverity="0"] {
-  background-color: rgb(0,0,10);
-  color: rgb(0,255,0);
-}
-"""
-
-STYLE_YELLOW = """
-PyDMLabel {
-  border-radius: 2px;
-}
-PyDMLabel[alarmSensitiveBorder="true"][alarmSeverity="0"] {
-  background-color: rgb(0,0,10);
-  color: rgb(255,255,0);
-}
-"""
-
 STAT_REG  = QFont('Sans Serif', 18)
-
-# stupid magic numbers because I can't find a FBCK HSTA bit decoder
-HSTA_FBCK_ON = 268601505
-HSTA_FBCK_COMP = 268599457
-
 
 class F2_WFH(Display):
 
@@ -88,7 +53,7 @@ class F2_WFH(Display):
 
         self.setWindowTitle('FACET-II work-from-home CUD')
 
-        self.SYAG_image = InvertedImage(
+        self.SYAG_image = InvertedPyDMImage(
             im_ch='CAMR:LI20:100:Image:ArrayData',
             w_ch='CAMR:LI20:100:Image:ArraySize0_RBV',
             parent=self.ui.frame_SYAG
@@ -97,7 +62,7 @@ class F2_WFH(Display):
         self.SYAG_image.colorMap = 1
         # SYAG_image.setGeometry(0,0, 340, 170)
 
-        self.VCCF_image = InvertedImage(
+        self.VCCF_image = InvertedPyDMImage(
             im_ch='CAMR:LT10:900:Image:ArrayData',
             w_ch='CAMR:LT10:900:Image:ArraySize0_RBV',
             parent=self.ui.frame_vcc
@@ -113,19 +78,36 @@ class F2_WFH(Display):
         #     xMin=170, xMax=1340, yMin=110, yMax=1000
         #     )
 
-        self.L0_msmt     = PyDMChannel(address=PV_L0_MSMT, value_slot=self.msmt_ts_L0)
-        self.L2_msmt     = PyDMChannel(address=PV_L2_MSMT, value_slot=self.msmt_ts_L2)
-        self.L3_msmt     = PyDMChannel(address=PV_L3_MSMT, value_slot=self.msmt_ts_L3)
-        self.IPWS1_msmt  = PyDMChannel(address=PV_IPWS1_MSMT, value_slot=self.msmt_ts_IPWS1)
-        self.IPBlen_msmt = PyDMChannel(address=PV_IPBlen_MSMT, value_slot=self.msmt_ts_IPBlen)
-        for ch in [self.L0_msmt, self.L2_msmt, self.L3_msmt, self.IPWS1_msmt, self.IPBlen_msmt]:
-            ch.connect()
-
+        # connect to emittance & S20 measurement PVs to update timestamps
         self.L0_msmt_PV     = get_pv(PV_L0_MSMT)
         self.L2_msmt_PV     = get_pv(PV_L2_MSMT)
         self.L3_msmt_PV     = get_pv(PV_L3_MSMT)
         self.IPWS1_msmt_PV  = get_pv(PV_IPWS1_MSMT)
         self.IPBlen_msmt_PV = get_pv(PV_IPBlen_MSMT)
+
+        self.L0_msmt = PyDMChannel(
+            address=PV_L0_MSMT,
+            value_slot=partial(self.msmt_ts, self.L0_msmt_PV, self.ui.ts_L0)
+            )
+        self.L2_msmt = PyDMChannel(
+            address=PV_L2_MSMT,
+            value_slot=partial(self.msmt_ts, self.L2_msmt_PV, self.ui.ts_L2)
+            )
+        self.L3_msmt = PyDMChannel(
+            address=PV_L3_MSMT,
+            value_slot=partial(self.msmt_ts, self.L3_msmt_PV, self.ui.ts_L3)
+            )
+        self.IPWS1_msmt = PyDMChannel(
+            address=PV_IPWS1_MSMT,
+            value_slot=partial(self.msmt_ts, self.IPWS1_msmt_PV, self.ui.ts_IPWS1)
+            )
+        self.IPBlen_msmt = PyDMChannel(
+            address=PV_IPBlen_MSMT,
+            value_slot=partial(self.msmt_ts, self.IPBlen_msmt_PV, self.ui.ts_IPBlen)
+            )
+
+        for ch in [self.L0_msmt, self.L2_msmt, self.L3_msmt, self.IPWS1_msmt, self.IPBlen_msmt]:
+            ch.connect()
 
         self.setup_injector()
         self.setup_L1()
@@ -139,16 +121,16 @@ class F2_WFH(Display):
         self.ui.fps_DTOTR2.setCurrentText('10')
         self.update_camera_FPS()
 
-        ind_XTCAVF = klysIndicator('20-4', parent=self.ui.cont_XTCAVF)
+        ind_XTCAVF = klysIndicator('20-4', parent=self.ui.cont_XTCAVF, mini=True)
         ind_XTCAVF.setGeometry(0,0,60,50)
 
         Qbunch_enable = bitStatusLabel(
             'SIOC:SYS1:ML03:AO502', word_length=1, bit=0, parent=self.ui.ind_qfb
             )
         Qbunch_enable.setGeometry(0,0,114,27)
-        ind_LI11 = F2SteeringFeedbackIndicator(
+        ind_LI11 = SCPSteeringFBIndicator(
             'LI11:FBCK:26:HSTA', parent=self.ui.ind_l2steer)
-        ind_LI18 = F2SteeringFeedbackIndicator(
+        ind_LI18 = SCPSteeringFBIndicator(
             'LI18:FBCK:28:HSTA', parent=self.ui.ind_l3steer)
         ind_LI11.setFont(STAT_REG)
         ind_LI18.setFont(STAT_REG)
@@ -163,34 +145,19 @@ class F2_WFH(Display):
     def update_camera_FPS(self):
         self.VCCF_image.maxRedrawRate = int(self.ui.fps_VCC.currentText())
         self.SYAG_image.maxRedrawRate = int(self.ui.fps_SYAG.currentText())
-        # self.ui.live_DTOTR2.maxRedrawRate = int(self.ui.fps_DTOTR2.currentText())
+        self.ui.live_DTOTR2.maxRedrawRate = int(self.ui.fps_DTOTR2.currentText())
         return
 
-    def msmt_ts_L0(self, value=None, char_value=None):
-        self.msmt_ts(self.L0_msmt_PV, self.ui.ts_L0, value=value, char_value=char_value)
-
-    def msmt_ts_L2(self, value=None, char_value=None):
-        self.msmt_ts(self.L2_msmt_PV, self.ui.ts_L2, value=value, char_value=char_value)
-        
-    def msmt_ts_L3(self, value=None, char_value=None, **kw):
-        self.msmt_ts(self.L3_msmt_PV, self.ui.ts_L3, value=value, char_value=char_value)
-
-    def msmt_ts_IPWS1(self, value=None, char_value=None, **kw):
-        self.msmt_ts(self.IPWS1_msmt_PV, self.ui.ts_IPWS1, value=value, char_value=char_value)
-
-    def msmt_ts_IPBlen(self, value=None, char_value=None, **kw):
-        self.msmt_ts(self.IPBlen_msmt_PV, self.ui.ts_IPBlen, value=value, char_value=char_value)
-
     def msmt_ts(self, PV_msmt_obj, label_obj, value=None, char_value=None, **kw):
-        """ temp kludge to update L2, L3 & IP Blen measurement timestamps """
+        """ sets the text of <label_obj> to the update timestamp of PV_msmt_obj """
         ts_str = str(dt.fromtimestamp(PV_msmt_obj.timestamp).strftime('%d-%b-%Y %H:%M'))
         label_obj.setText(ts_str)
 
     def setup_injector(self):
-        ind_gun   = klysIndicator('10-3', parent=self.ui.cont_gun_2)
-        ind_L0A   = klysIndicator('10-8', parent=self.ui.cont_L0A_2)
-        ind_L0B   = klysIndicator('10-4', parent=self.ui.cont_L0B_2)
-        ind_TCAV0 = klysIndicator('10-5', parent=self.ui.cont_TCAV0_2)
+        ind_gun   = klysIndicator('10-3', parent=self.ui.cont_gun_2, mini=True)
+        ind_L0A   = klysIndicator('10-8', parent=self.ui.cont_L0A_2, mini=True)
+        ind_L0B   = klysIndicator('10-4', parent=self.ui.cont_L0B_2, mini=True)
+        ind_TCAV0 = klysIndicator('10-5', parent=self.ui.cont_TCAV0_2, mini=True)
 
         for ind in [ind_gun, ind_L0A, ind_L0B, ind_TCAV0]:
             ind.setGeometry(0,0,60,50)
@@ -198,18 +165,16 @@ class F2_WFH(Display):
 
     def setup_L1(self):
         ind_L1SA = klysIndicator(
-            '11-1', pv_pdes='KLYS:LI11:11:PREQ', parent=self.ui.cont_L1SA
+            '11-1', pv_pdes='KLYS:LI11:11:PREQ', parent=self.ui.cont_L1SA, mini=True
             )
         ind_L1SB = klysIndicator(
-            '11-2', pv_pdes='KLYS:LI11:21:PREQ', parent=self.ui.cont_L1SB
+            '11-2', pv_pdes='KLYS:LI11:21:PREQ', parent=self.ui.cont_L1SB, mini=True
             )
-
         for ind in [ind_L1SA, ind_L1SB]:
             ind.setGeometry(0,0,60,50)
         return
 
     def setup_L2_L3(self):
-
         for linac, container in zip([L2, L3], [self.ui.area_L2, self.ui.area_L3]):
             L = QGridLayout()
             L.setSpacing(5)
@@ -218,38 +183,12 @@ class F2_WFH(Display):
 
             for s in linac:
                 i_sbst = int(s)-11
-                sbst = sbstIndicator(s)
+                sbst = sbstIndicator(s, mini=True)
                 L.addWidget(sbst, 0, i_sbst)
 
                 for k in KLYSTRONS:
                     # exclude 11-1 & 2
                     if s == '11' and k in ['1','2']: continue
-                    klys = klysIndicator(f'{s}-{k}')
+                    klys = klysIndicator(f'{s}-{k}', mini=True)
                     L.addWidget(klys, int(k), i_sbst)
         return
-
-
-# subclass to flip iamge in X/Y - performance intensive :(
-class InvertedImage(PyDMImageView):
-    def __init__(self, im_ch, w_ch, parent=None, args=None):
-        PyDMImageView.__init__(self, parent=parent, image_channel=im_ch, width_channel=w_ch)
-
-    def process_image(self, image): return flip(image)
-
-class F2SteeringFeedbackIndicator(PyDMLabel):
-    """ checks FBCK hardware status to check for feedback enable/compute """
-
-    def __init__(self, init_channel, parent=None, args=None):
-        PyDMLabel.__init__(self, init_channel=init_channel, parent=parent)
-        self.setAlignment(AlignCenter)
-
-    def value_changed(self, new_value):
-        PyDMLabel.value_changed(self, new_value)
-        if new_value == HSTA_FBCK_ON:    
-            self.setText('ON')
-            self.setStyleSheet(STYLE_GREEN)
-        elif new_value == HSTA_FBCK_COMP:
-            self.setText('Comp.')
-            self.setStyleSheet(STYLE_YELLOW)
-        else:                            
-            self.setText('Off/Sample')
